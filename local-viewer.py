00d2bc9ee1c4be1f19d186cde70aa12a83b6d5e4
Aechs <89534947+Aechs@users.noreply.github.com>
2021-09-02 10:17:34 -0500

    add text/css

--- local-viewer.py
+++ local-viewer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import os
-import sys
 import platform
 import webbrowser
 
 if platform.system() == "Linux":
     os.system('/usr/bin/python3 -m http.server 8080')
     # sys.stdout( )
-elif platform.system() == "Windows":
-    os.system('C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python39\python.exe -m http.server 8080')
+else:
+    os.system('python -m http.server 8080')
 
 webbrowser.open('http://127.0.0.1:8080/index.html')
