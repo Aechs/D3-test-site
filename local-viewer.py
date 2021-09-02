baa477bbf3035d20436e167727a7ec2803fc5afa
Aechs <89534947+Aechs@users.noreply.github.com>
2021-09-02 10:20:03 -0500

    css tweak

--- local-viewer.py
+++ local-viewer.py
@@ -4,9 +4,9 @@ import platform
 import webbrowser
 
 if platform.system() == "Linux":
-    os.system('/usr/bin/python3 -m http.server 8080')
+    os.system('/usr/bin/python3 -m http.server 8000')
     # sys.stdout( )
 else:
-    os.system('python -m http.server 8080')
+    os.system('python -m http.server 8000')
 
-webbrowser.open('http://127.0.0.1:8080/index.html')
+webbrowser.open('http://127.0.0.1:8000/index.html')
